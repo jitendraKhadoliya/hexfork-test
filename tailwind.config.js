59b108c98de259c7fc1de33da70b22fa5f33f18c
jitendra <jitendrasaini352jjnu@gmail.com>
2023-06-14 18:12:41 +0530

    added nested css

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,10 @@ module.exports = {
       },
     },
   },
-  // plugins: [],
-  plugins: {
-    "postcss-import": {},
-    "tailwindcss/nesting": {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss/nesting"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+  ],
 };
